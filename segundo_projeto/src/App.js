ed8356371cc9ccda3b968b1fb40a4cfa417a8e93
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-09-16 15:40:16 -0300

    Refere-se a primeira versão de funcionalidade do app `Clima Brasileiro`

--- segundo_projeto/src/App.js
+++ segundo_projeto/src/App.js
@@ -1,6 +1,46 @@
+import React, { useState } from "react";
+import dados from "./dados";
+import "./App.css";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredData = dados.filter((item) =>
+    item.nome.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <h1>Second Project</h1>
+    <div className="titulo">
+      <h1>Clima Brasileiro</h1>
+      <div className="pesquisa">
+        <input
+          type="text"
+          required
+          placeholder="Buscar por cidade..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+      {searchTerm && (
+        <ul className="lista">
+          {filteredData.length > 0 ? (
+            filteredData.map((item, index) => (
+              <div className="resultado">
+                <li key={index}>
+                  {item.nome} - {item.temp} - {item.previsao}
+                </li>
+              </div>
+            ))
+          ) : (
+            <li>Nenhum resultado encontrado</li>
+          )}
+        </ul>
+      )}
+    </div>
   );
 }
 
